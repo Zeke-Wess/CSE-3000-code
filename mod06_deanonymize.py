3ed8f69276adff166f493e057c01d1c4f9cadfd8
Zeke Wessell <ithezeke@gmail.com>
2026-03-10 06:27:24 -0400

    completed mod 6

--- mod06_deanonymize.py
+++ mod06_deanonymize.py
@@ -9,7 +9,7 @@ def load_data(anonymized_path, auxiliary_path):
     return anon, aux
 
 
-def link_records(anon_df, aux_df):
+def link_records(anon_df:pd.DataFrame, aux_df:pd.DataFrame):
     """
     Attempt to link anonymized records to auxiliary records
     using exact matching on quasi-identifiers.
@@ -18,12 +18,17 @@ def link_records(anon_df, aux_df):
       anon_id, matched_name
     containing ONLY uniquely matched records.
     """
-    raise NotImplementedError
+    
+    anon_df.drop_duplicates(subset=["age", "zip3", "gender"], keep=False) # Remove any duplicates 
+    aux_df.drop_duplicates(subset=["age", "zip3", "gender"], keep=False)
+    anon_df.sort_values(by=["age", "zip3", "gender"])       # Sort so the names will line up 
+    aux_df.sort_values(by=["age", "zip3", "gender"])
+    matches = pd.DataFrame({"anon_id": anon_df["anon_id"], "matched_name" : aux_df["name"]})
+    return matches
 
-
-def deanonymization_rate(matches_df, anon_df):
+def deanonymization_rate(matches_df:pd.DataFrame, anon_df:pd.DataFrame):
     """
     Compute the fraction of anonymized records
     that were uniquely re-identified.
     """
-    raise NotImplementedError
+    return (matches_df.size // 2) / (anon_df.size // 2)
