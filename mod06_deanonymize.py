4f05d4c4219454312fca8c5f8fb50bdde3142daf
Zeke Wessell <ithezeke@gmail.com>
2026-03-10 06:32:06 -0400

    Updated mod 6

--- mod06_deanonymize.py
+++ mod06_deanonymize.py
@@ -19,11 +19,11 @@ def link_records(anon_df:pd.DataFrame, aux_df:pd.DataFrame):
     containing ONLY uniquely matched records.
     """
     
-    anon_df.drop_duplicates(subset=["age", "zip3", "gender"], keep=False) # Remove any duplicates 
-    aux_df.drop_duplicates(subset=["age", "zip3", "gender"], keep=False)
-    anon_df.sort_values(by=["age", "zip3", "gender"])       # Sort so the names will line up 
-    aux_df.sort_values(by=["age", "zip3", "gender"])
-    matches = pd.DataFrame({"anon_id": anon_df["anon_id"], "matched_name" : aux_df["name"]})
+    unique_anon = anon_df.drop_duplicates(subset=["age", "zip3", "gender"], keep=False) # Remove any duplicates 
+    unique_aux = aux_df.drop_duplicates(subset=["age", "zip3", "gender"], keep=False)
+    unique_anon.sort_values(by=["age", "zip3", "gender"], inplace=True)       # Sort so the names will line up 
+    unique_aux.sort_values(by=["age", "zip3", "gender"], inplace=True)
+    matches = pd.DataFrame({"anon_id": unique_anon["anon_id"], "matched_name" : unique_aux["name"]})
     return matches
 
 def deanonymization_rate(matches_df:pd.DataFrame, anon_df:pd.DataFrame):
@@ -31,4 +31,4 @@ def deanonymization_rate(matches_df:pd.DataFrame, anon_df:pd.DataFrame):
     Compute the fraction of anonymized records
     that were uniquely re-identified.
     """
-    return (matches_df.size // 2) / (anon_df.size // 2)
+    return (matches_df["anon_id"].size) / (anon_df["anon_id"].size)
